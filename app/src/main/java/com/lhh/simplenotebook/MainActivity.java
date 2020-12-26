cd8283665fe662cf487ac5dc4abbaf4957177739
lhhboy <1603614324@qq.com>
2020-12-26 20:58:09 +0800

    注册界面

--- app/src/main/java/com/lhh/simplenotebook/MainActivity.java
+++ app/src/main/java/com/lhh/simplenotebook/MainActivity.java
@@ -3,12 +3,18 @@ package com.lhh.simplenotebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText et_username;
+    private EditText et_password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
